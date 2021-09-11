a23d3dc1995afe63064f229db6b50f4b06bdab72
aanyan <a.anyan@zoovu.com>
2021-09-11 19:17:59 +0200

    tt

--- src/pages/index.php
+++ src/pages/index.php
@@ -5,6 +5,7 @@
 
 <div>
 <h1> This is the index file </h1>
+<h1> This is the index file </h1>
 
 <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis, sint!</h2>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo facilis dolore possimus cupiditate obcaecati et, cumque expedita tempore, nihil id, nulla sint deserunt dicta aliquam quo eum ratione laudantium quidem.</p>
